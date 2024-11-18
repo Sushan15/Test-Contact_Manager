8f52b4dd19611e2ffde276bedc13ba311731f7f1
Sushan15 <108874361+Sushan15@users.noreply.github.com>
2024-11-18 21:31:39 +0530

    Create Contact.java

---
+++ contactManagementSystem/Contact.java
@@ -0,0 +1,15 @@
+package contactManagementSystem;
+
+public class Contact {
+	public String firstName;
+	public String lastName;
+	public long contactNumber;
+	public Contact(String firstName, String lastName, long contactNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.contactNumber = contactNumber;
+	}
+	
+	
+}
